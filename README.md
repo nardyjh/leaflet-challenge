fb4055bbae7a9d70154635438240f1d12118563e
Jorge Nardy <nardyjh@gmail.com>
2023-12-02 21:47:33 -0500

    project complete

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# leaflet-challenge
+# USGS Earthquake Visualization
+
+## Overview
+
+The USGS Earthquake Visualization project offers an interactive map-based representation of earthquake data collected by the United States Geological Survey (USGS). This tool provides a means to visualize and explore earthquake events, offering insights into their magnitudes, depths, and locations globally.
+
+## Getting Started
+
+### Prerequisites
+
+To run the visualization locally, ensure you have the following dependencies:
+
+- Leaflet 
+- D3.js 
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/nardyjh/leaflet-challenge.git
+    ```
+
+2. Open index.html in a web browser to view the earthquake visualization.
+
+## Features
+
+- Map Representation: Utilizes Leaflet to display earthquake events on an interactive map.
+
+- Marker Styling: Markers are sized based on earthquake magnitude, and colors represent the depth, with darker shades indicating greater depth.
+
+- Popups: Clickable markers provide detailed information about each earthquake.
+
+## Acknowledgments
+This project is developed as a visualization tool for USGS earthquake data for educational purposes with the University of Toronto and is not officially affiliated with the United States Geological Survey.
+
+## References 
+- [United States Geological Survey](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+- [Add Legend to Leaflet Map](https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map)
+- [Linear Scales](https://d3js.org/d3-scale/linear)
+- [D3 Scales](https://observablehq.com/@d3/d3-scalelinear)
--- static/js/logic.js
+++ static/js/logic.js
@@ -4,75 +4,69 @@ let myMap = L.map("map", {
     zoom: 2,
 });
 
-// Add a tile layer for the base map
+// Add tile layer for base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Function to determine radius based on magnitude
+// Function for radius based on magnitude
 function getRadius(magnitude) {
-    // You can customize this function to map magnitude to a specific radius scale
-    // For example, using a linear scale for simplicity
     return Math.sqrt(magnitude) * 5;
-}
-
-// Function to determine color based on depth
-function getColor(depth) {
-    // You can customize this function to map depth to a specific color scale
-    // For example, using a gradient from light to dark based on depth values
-    return depth > 90 ? 'darkred' :
-           depth > 70 ? 'red' :
-           depth > 50 ? 'orange' :
-           depth > 30 ? 'yellow' :
-           depth > 10 ? 'green' :
-           depth > -10 ? 'lightgreen' :
-                       'lightblue';
 }
 
 // Function to create legend
 function createLegend() {
-    // Define legend values based on depth thresholds
-    const legendValues = [-10, 10, 30, 50, 70, 90];
+    // Legend values based on depth 
+    let legendValues = [-10, 10, 30, 50, 70, 90];
 
-    // Create SVG element for the legend
-    let svg = d3.select("body")  // You can select any container element or the body
-        .append("svg")
-        .attr("class", "legend");
-
-    // Use the same color scale for legend and markers
+    // Use same colors for legend and markers
     let legendColorScale = d3.scaleLinear()
         .domain([legendValues[0], legendValues[legendValues.length - 1]])
-        .range(["lightblue", "darkred"]);
+        .range(["yellow", "darkred"]);
 
-    // Create legend rectangles
-    svg.selectAll("rect")
-        .data(legendValues)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * 30)
-        .attr("y", 0)
-        .attr("width", 30)
-        .attr("height", 10)
-        .style("fill", d => legendColorScale(d));
+    // Create Leaflet control for legend
+    let legend = L.control({ position: 'bottomright' });
 
-    // Add legend labels
-    svg.selectAll("text")
-        .data(legendValues)
-        .enter()
-        .append("text")
-        .attr("x", (d, i) => i * 30)
-        .attr("y", 20)
-        .text(d => d.toFixed(2)); // Format the legend values as needed
+    // Function to update legend
+    legend.onAdd = function () {
+        let div = L.DomUtil.create('div', 'legend');
+        
+        // Add white background 
+        div.style.backgroundColor = 'white';
+        div.style.padding = '10px';
+
+        // Create legend colors and labels
+        legendValues.forEach((value, index) => {
+            div.innerHTML +=
+                `<div style="background-color:${legendColorScale(value)}; width:30px; height:10px; display:inline-block;"></div>` +
+                `<div style="display:inline-block; padding-left: 5px; padding-right: 10px;">${index === legendValues.length - 1 ? `${value}+` : `${value}-${legendValues[index + 1]}`}</div><br>`;
+        });
+        return div;
+    };
+
+    // Add legend to map
+    legend.addTo(myMap);
+
+    // Return color for later access
+    return legendColorScale;
+}
+
+// Function to determine color based on depth
+function getColor(depth, legendColorScale) {
+    return legendColorScale(depth);
 }
 
 // Define URL for earthquake data
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Use D3 to load and parse the GeoJSON data
+// Use D3 to load and parse GeoJSON data
 d3.json(url).then((data) => {
+    // Call createLegend function after adding markers 
+    let legendColorScale = createLegend();
+
     // Loop through features and create markers
     data.features.forEach((feature) => {
-        // Extract properties from the feature
+        // Find data in features for markers
         let coordinates = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
         let magnitude = feature.properties.mag;
         let depth = feature.geometry.coordinates[2];
@@ -81,17 +75,14 @@ d3.json(url).then((data) => {
         let marker = L.circleMarker(coordinates, {
             radius: getRadius(magnitude),
             color: "black",
-            fillColor: getColor(depth),
-            fillOpacity: 0.7,
+            fillColor: getColor(depth, legendColorScale),
+            fillOpacity: 0.8,
         });
 
-        // Bind a popup with additional information
+        // Bind popup with information
         marker.bindPopup(`<b>Magnitude:</b> ${magnitude}<br><b>Depth:</b> ${depth}`);
 
-        // Add the marker to the map
+        // Add marker to map
         marker.addTo(myMap);
     });
-
-    // Call the createLegend function after adding markers to the map
-    createLegend();
 });
